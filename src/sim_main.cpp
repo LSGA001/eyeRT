34fb2964f7073bb3a0391f49e92bdf4ae1795647
LSGA001 <leo.ga@live.com>
2022-02-08 23:59:41 -0600

    Minor modifications

--- src/sim_main.cpp
+++ src/sim_main.cpp
@@ -7,13 +7,23 @@
 int main() {
 
 	/* Load config
-	   and parameters */
-
+	  	and parameters */
 	//load_config();
 	
 	/* Initialize GRIN matrix */
-
 	//make_grin();
+	
+	/* Initialize rays */
+	//make_rays();
+	
+	/* Calculate gradient */
+	//get_gradient();
+	
+	/* Ray propagation */
+	//propagate_rays();
+	 
+	/* Visualization */
+	//plots();
 
 	return 0;
 }
